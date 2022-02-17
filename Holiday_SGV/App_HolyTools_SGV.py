0d37bb93b96f941aa0a80b84f94a60fe069dd8c5
Sergio García <98185143+sergigavi@users.noreply.github.com>
2022-02-18 00:00:47 +0100

    Initial commit

---
+++ Holiday_SGV/App_HolyTools_SGV.py
@@ -0,0 +1,143 @@
+
+#coding: utf-8
+
+'''
+Created on 15 feb 2022
+
+@author: Sergio
+'''
+
+import tkinter as tk
+from tkinter import ttk,messagebox, scrolledtext
+from PIL.ImageTk import PhotoImage
+from Holiday_SGV.DDBB import DDBB
+
+#
+
+baseDeDatos = DDBB()
+
+mainVentana = tk.Tk()
+mainVentana.title("Holiday Tools - Sergio García - 2ºDAM")
+mainVentana.iconbitmap('icono.ico')
+mainVentana.geometry("375x275")
+mainVentana.resizable(False, False)
+
+cMostrar:bool = False;
+
+#
+
+def mostrarContrasenna():
+    global cMostrar, entryContrasenna
+    
+    if not cMostrar:
+        cMostrar = True
+    else:
+        cMostrar = False
+    
+    if cMostrar:
+        entryContrasenna.config(show="")
+    else:
+        entryContrasenna.config(show="*")
+        
+def comprobarUsuario():
+    existe:bool = False
+    
+    global txtUsuario
+    
+    for i in range(baseDeDatos.getNumeroUsuarios()):
+        userActual = baseDeDatos.hacerQuery("SELECT Usuario FROM usuarios")[i][0]
+        if userActual == txtUsuario.get():
+            print(userActual)
+            existe = True
+            
+        
+    return existe
+
+def comprobarContrasenna():
+    
+    global txtUsuario, txtContrasenna
+    
+    contrasennaCoincide:bool = False
+    
+    contrasennabuena = baseDeDatos.hacerQuery("SELECT Contraseña FROM usuarios WHERE Usuario LIKE '" + txtUsuario.get() +"'")
+    
+    if txtContrasenna.get() == contrasennabuena:
+        print("contrasenna coincide")
+        contrasennaCoincide = True
+    
+    return contrasennaCoincide
+    
+def iniciarSesion():
+    
+    if not comprobarUsuario():
+        messagebox.showerror("Usuario erróneo", "El usuario introducido no está registrado")
+    else:
+        if comprobarContrasenna():
+            messagebox.showinfo("Login succesful", "Inicio de sesion correcto")
+        else:
+            messagebox.showerror("Contraseña errónea", "La contraseña introducida no coincide")
+        
+        
+    
+
+def heOlvidadoMiContrasenna():
+    pass
+
+#
+
+lblUsuario = ttk.Label(
+    mainVentana,
+    text="Usuario",
+    font=("Helvetica", 16),
+    foreground="#0B67D9")
+lblUsuario.grid(row=0, column=0, padx=10, pady=(15,3), sticky="W")
+
+txtUsuario = tk.StringVar()
+entryUsuario = ttk.Entry(mainVentana, textvariable=txtUsuario, width=38)
+entryUsuario.grid(row=1, column=0, padx=10, pady=0)
+
+
+lblContrasenna = ttk.Label(
+    mainVentana,
+    text="Contraseña",
+    font=("Helvetica", 16),
+    foreground="#0B67D9")
+lblContrasenna.grid(row=2, column=0, padx=10,pady=(15,3), sticky="W")
+ 
+
+txtContrasenna = tk.StringVar()
+entryContrasenna = ttk.Entry(mainVentana, show="*", textvariable=txtContrasenna, width=38)
+entryContrasenna.grid(row=3, column=0, padx=10, pady=0)
+
+
+f = tk.PhotoImage(file = "verContrasenna.png")
+fotoBoton = f.subsample(20, 20) 
+
+btnMostrarContraseña = ttk.Button(mainVentana, image=fotoBoton, command=mostrarContrasenna)
+btnMostrarContraseña.grid(row=3, column=1, padx=10, pady=10)
+
+
+frameBotones = ttk.Frame(mainVentana)
+frameBotones.grid(row=4, column=0, padx=10, pady=10)
+
+btnEntrar = ttk.Button(frameBotones, text="Entrar", width=30, command=iniciarSesion)
+btnEntrar.grid(row=0, column=0, padx=10, pady=10)
+
+btnContrasennaOlvidada = ttk.Button(frameBotones, text="He olvidado mi contraseña", width=30, command=heOlvidadoMiContrasenna)
+btnContrasennaOlvidada.grid(row=1, column=0, padx=10, pady=2)
+
+
+
+
+#
+
+mainVentana.mainloop()
+
+
+
+
+
+
+
+
+
---
+++ Holiday_SGV/DDBB.py
@@ -0,0 +1,82 @@
+
+#coding: utf-8
+
+'''
+Created on 17 feb 2022
+
+@author: Sergio
+'''
+
+
+import mysql.connector as conn
+import tkinter
+from tkinter import ttk, messagebox
+
+class DDBB():
+    
+    def __init__(self):
+        self.conexion = conn.connect(host="127.0.0.1", user="root", password="123abc")
+        self.cursor = self.conexion.cursor()
+        self.cursor.execute("CREATE DATABASE IF NOT EXISTS HolydayToolsDDBB")
+        self.cursor.execute("USE HolydayToolsDDBB")
+        self.cargarDatos()
+
+    def getNumeroUsuarios(self):
+        self.cursor.execute("SELECT COUNT(usuarios.Usuario) FROM usuarios")
+        return int(self.cursor.fetchall()[0][0])
+    
+    def hacerQuery(self, query):
+        self.cursor.execute(query)
+        return self.cursor.fetchall()
+    
+    def cargarDatos(self):
+        
+        #Creo las tablas
+        
+        self.cursor.execute('''CREATE TABLE IF NOT EXISTS usuarios(
+        Usuario VARCHAR(30) NOT NULL PRIMARY KEY,
+        Contraseña VARCHAR(30) NOT NULL,
+        Tipo VARCHAR(13)
+        
+        )ENGINE=InnoDB;
+        ''')
+        
+        self.cursor.execute('''CREATE TABLE IF NOT EXISTS fechas(
+        Fecha CHAR(10) NOT NULL,
+        Usuario VARCHAR(30) NOT NULL,
+        FOREIGN KEY (Usuario) REFERENCES usuarios(Usuario)
+        
+        )ENGINE=InnoDB;
+        ''')
+        
+        #Inserto datos
+        
+        self.cursor.execute('''INSERT INTO usuarios VALUES ('admin','admin','administrador');''')
+        self.cursor.execute('''INSERT INTO usuarios VALUES ('MarioSantos','123abc','administrador');''')
+        self.cursor.execute('''INSERT INTO usuarios VALUES ('Sergio','123abc','administrador');''')
+        self.cursor.execute('''INSERT INTO usuarios VALUES ('Pizarroso','vegano','empleado');''')
+        self.cursor.execute('''INSERT INTO usuarios VALUES ('Jorge','admin','empleado');''')
+        
+        
+        self.conexion.commit()
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
